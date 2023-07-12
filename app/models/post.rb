47ae2e18308f572102bd99ad91b08ceb482a5d6f
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-12 20:30:52 +0530

    Add recents and updates methods for the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ApplicationRecord
 
   has_many :comments
   has_many :likes
+
+  def recent_comments
+    comments.order('created_at Desc').limit(5)
+  end
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
 end
