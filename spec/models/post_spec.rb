7afd3789c0498c31243687226dc4142b4b99698f
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-14 00:13:45 +0530

    Generate Post_spec & add test cases

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  subject { Post.create(Title: 'Title', Text: 'Text', AuthorId: 1) }
+  before { subject.save }
+  
+  it 'Title is present' do
+    subject.Title = nil
+    expect(subject).to_not be_valid
+  end
+  it 'Text is present' do
+    subject.Text = nil
+    expect(subject).to_not be_valid
+  end
+  it 'AuthorId is present' do
+    subject.AuthorId = nil
+    expect(subject).to_not be_valid
+  end
+end
