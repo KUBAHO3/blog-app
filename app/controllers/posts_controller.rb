9d891fa192cf541f19385f7221a25ebcc09e9a39
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-19 19:28:46 +0530

    Add set user in post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,16 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
+  before_action :set_user, only: %i[index show]
 
   def index
     @posts = Post.all
   end
 
   def show; end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 
   def set_post
     @posts = Post.find(params[:id])
