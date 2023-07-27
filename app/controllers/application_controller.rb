06009dbe02b2d08ce50a259de1a5495eb7802304
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-27 12:18:05 +0530

    Update perimeters of controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo, :bio])
+  end
 end
