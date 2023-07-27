b85a7ad7b53c3d0e549381dd9d2770fd3b6ecc6c
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-27 12:25:11 +0530

    Update controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:home]
   before_action :set_user, only: [:show]
+
+  def home; end
 
   def index
     @users = User.all
