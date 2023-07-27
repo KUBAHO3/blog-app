ffa8e9d3f2357e8a028b5bfc042f9256acaccc17
ikoote1 <ikootepreim@gmail.com>
2023-07-27 11:38:14 +0300

    blog authentication with devise

--- db/schema.rb
+++ db/schema.rb
@@ -44,20 +44,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_061650) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.text "bio"
-    t.integer "postsCounter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+  # create_table "users", force: :cascade do |t|
+  #   t.string "name"
+  #   t.string "photo"
+  #   t.text "bio"
+  #   t.integer "postsCounter"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  #   t.string "email", default: "", null: false
+  #   t.string "encrypted_password", default: "", null: false
+  #   t.string "reset_password_token"
+  #   t.datetime "reset_password_sent_at"
+  #   t.datetime "remember_created_at"
+  #   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  # end
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
