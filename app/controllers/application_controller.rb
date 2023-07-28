c5cbf4cf0997af605acc64cf778518653663a132
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-28 14:47:49 +0530

    Add roles to the users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :bio])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo, :bio])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio role])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio role])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  ROLES = %w[admin user].freeze
+
   def most_recent_three_posts
     posts.order(created_at: :desc).limit(3)
   end
